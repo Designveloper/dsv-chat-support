46ca9724c4ba0f7d8305e7fa29834e6e98fff8cb
ntpd164 <duongntp@dgroup.co>
2025-04-03 15:24:26 +0700

    fix: api url not working

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.API_URL || 'http://localhost:3000';
+const API_URL = "https://chat-support-server.onrender.com";
 
 interface AuthResponse {
     accessToken: string;
