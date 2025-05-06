e6750f4a0a63571c29395aa09799839df9c59b14
ntpd164 <duongntp@dgroup.co>
2025-05-06 21:03:50 +0700

    fix: logo position and add social media links

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "./Header";
 import "./Home.scss";
+import companyLogo from "../assets/company-logo.png";
+import githubLogo from "../assets/github-logo.png";
+import linkedinLogo from "../assets/linkedin-logo.png";
 
 const Home: React.FC = () => {
   return (
@@ -13,17 +16,47 @@ const Home: React.FC = () => {
             <p className="home__subtitle">
               Chat with visitors directly from Slack and never miss a message.
             </p>
-            {/* copyright with company website url */}
-            <p className="home__copyright">
-              &copy; {new Date().getFullYear()} DSV. All rights reserved.
+            <a
+              href="https://www.designveloper.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="home__link"
+            >
+              <img src={companyLogo} alt="Logo" className="home__logo" />
+            </a>
+
+            <div className="home__social-links">
               <a
-                href="https://www.designveloper.com/"
+                href="https://github.com/Designveloper/dsv-chat-support"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="home__link"
+                className="home__social-link"
               >
-                DSV
+                <img
+                  src={githubLogo}
+                  alt="GitHub"
+                  className="home__social-icon"
+                />
+                <span className="home__social-text">GitHub</span>
               </a>
+              <a
+                href="https://www.linkedin.com/company/designveloper"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="home__social-link"
+              >
+                <img
+                  src={linkedinLogo}
+                  alt="LinkedIn"
+                  className="home__social-icon"
+                />
+                <span className="home__social-text">LinkedIn</span>
+              </a>
+            </div>
+
+            <p className="home__copyright">
+              &copy; {new Date().getFullYear()} Designveloper - DSV TECH AND APP
+              CORP. All rights reserved.
             </p>
           </div>
         </section>
